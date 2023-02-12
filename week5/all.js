6eddb7e7c7a5eb3bd64ada1a98a5c8cf63aa5dbb
johnson <you@example.com>
2023-02-12 21:09:06 +0800

    week5 finish

--- week5/all.js
+++ week5/all.js
@@ -1,3 +1,4 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2'; 
 const apiPath = 'deliciousfood';
 Object.keys(VeeValidateRules).forEach(rule => {
